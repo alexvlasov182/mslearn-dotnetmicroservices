b1315cfd3921806b0350ed9a443819c601bea3d4
Alex Vlasov <drumlife182@gmail.com>
2023-03-14 14:10:24 +0100

    Worked with docker && microservice

--- backend/PizzaInfo.cs
+++ backend/PizzaInfo.cs
@@ -2,14 +2,14 @@ using System;
 
 namespace backend
 {
-    public class PizzaInfo
-    {
-        public string PizzaName { get; set; }
+  public class PizzaInfo
+  {
+    public string PizzaName { get; set; }
 
-        public int Cost { get; set; }
+    public int Cost { get; set; }
 
-        public string Ingredients { get; set; }
+    public string Ingredients { get; set; }
 
-        public string InStock { get; set; }
-    }
+    public string InStock { get; set; }
+  }
 }
